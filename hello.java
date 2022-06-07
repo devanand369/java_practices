74f94b3565d2e9b638996016805e6010f1a587c4
Devanand Yadav <63476064+devanand369@users.noreply.github.com>
2022-06-07 12:32:24 +0000

    some jave file added

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    /** This is a simple java hello world program which prints hello world.*/
+    public static void main(String args[]) {
+        System.out.println("Hello world\n Welcome to Java");
+        }
+}
